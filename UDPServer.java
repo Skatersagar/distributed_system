e191f0dc1fab8ac1cef437f9e0c869101147afc4
Skatersagar <skatersagar@gmail.com>
2016-04-23 20:58:24 +0530

    setup

--- UDPServer.java
+++ UDPServer.java
@@ -16,6 +16,22 @@ public class UDPServer {
 		try { port = Integer.parseInt(args[0]); } catch ( Exception e ) {
 	    System.out.println("Usage: java UDPServer [port]");
 	    System.exit(-1);
+
+	    try {
+	    bbuff = new byte[512];
+	    DGSock = new DatagramSocket(port);
+	    packetDock = new DatagramPacket(bbuff, bbuff.length);
+	    try {
+		DGSock.receive(packetDock);
+		DGSock.setSoTimeout(1000);
+		previouslyReceived = ProtogramPacket.fromDatagram(packetDock);	    
+		filename = ((new String(previouslyReceived.payload)).split(" ")[0]);
+		filesize = Long.parseLong((new String(previouslyReceived.payload)).split(" ")[1]);
+		dataOut = (OutputStream)(new FileOutputStream(new File(filename)));
+	    } catch ( Exception e ) {
+		System.out.println("Received bad file descriptor!\nTerminating program...");
+		System.exit(-1);
+	    }
 	}
     }
 }
