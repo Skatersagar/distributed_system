1d02b1a4d2347cf00698636fb1d9cc1c7726a493
Skatersagar <skatersagar@gmail.com>
2016-04-23 20:54:34 +0530

    Variable and objects created

--- UDPServer.java
+++ UDPServer.java
@@ -3,7 +3,15 @@ import java.io.*;
 
 public class UDPServer {    
     public static void main (String[] args) {
-
+    	DatagramSocket DGSock = null;
+		OutputStream dataOut = null;
+		ProtogramPacket previouslySent = new ProtogramPacket();;
+		ProtogramPacket previouslyReceived = new ProtogramPacket();
+		DatagramPacket packetDock = null;
+		byte[] bbuff = null;
+		String filename = "";
+		int port = 0;
+		long filesize = 0;
 
     }
 }
